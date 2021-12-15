3083a9e9b4bba3b03748d2d26e7c59acb4603f6c
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-15 18:24:27 -0500

    Remove redundant code

--- dstore/cache/bulk/import.sh
+++ dstore/cache/bulk/import.sh
@@ -32,11 +32,4 @@ for directory in cache/bulk/data/*; do
   done
 done
 
-sqlite3 db/dstore.sqlite ".read sql/clean-records-no-delete.sql" && \
-sqlite3 db/dstore.sqlite ".read sql/create-indexes.sql";
-
-
-cd ../../ingest/assistance/iati/sqlite/create-scripts/ && \
-for FILE in *; do sqlite3 ../../../../../D-Portal-Tracking/dstore/db/dstore.sqlite ".read $FILE"; done
-
 cd $ORIGDIR;

4c858bf2353ce5d4a72e113dec5aab1851d6b757
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-15 18:19:44 -0500

    Add post-import.sh

---
+++ dstore/cache/bulk/post-import.sh
@@ -0,0 +1,12 @@
+BASEDIR=$(dirname $0)
+ORIGDIR=${PWD}
+cd $BASEDIR;
+
+sqlite3 db/dstore.sqlite ".read sql/clean-records-no-delete.sql" && \
+sqlite3 db/dstore.sqlite ".read sql/create-indexes.sql";
+
+
+cd ../../ingest/assistance/iati/sqlite/create-scripts/ && \
+for FILE in *; do sqlite3 ../../../../../D-Portal-Tracking/dstore/db/dstore.sqlite ".read $FILE"; done
+
+cd $ORIGDIR;

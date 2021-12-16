470a83f25beec86ecfcca3a689dc213274586f65
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-16 09:08:38 -0500

    Change to correct directory

--- dstore/cache/bulk/post-import.sh
+++ dstore/cache/bulk/post-import.sh
@@ -2,9 +2,10 @@ BASEDIR=$(dirname $0)
 ORIGDIR=${PWD}
 cd $BASEDIR;
 
+cd ../..
+
 sqlite3 db/dstore.sqlite ".read sql/clean-records-no-delete.sql" && \
 sqlite3 db/dstore.sqlite ".read sql/create-indexes.sql";
-
 
 cd ../../ingest/assistance/iati/sqlite/create-scripts/ && \
 for FILE in *; do sqlite3 ../../../../../D-Portal-Tracking/dstore/db/dstore.sqlite ".read $FILE"; done
